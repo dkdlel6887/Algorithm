f35f067753d1a7315a8a491a4683ae590593006b
Yeonghan Kim <dkdlel6887@naver.com>
2023-02-06 20:28:26 +0900

    Programmers 튜플 구현

--- Programmers/PRGMS_튜플.py
+++ Programmers/PRGMS_튜플.py
@@ -1,21 +1,16 @@
-# 셀수있는 수량의 순서있는 열거 또는 어떤 순서를 따르는 요소들의 모음을 튜플(tuple)
-#  {  }  ,
-
-# 길이가 짧은 집합부터 앞쪽에 위치, 새로나오는 숫자들은 뒤에 추가, 한번에 두개 문자가 새로 추가 X
-s.split(',') > {a},{d,a,c,b},{a,b},{c,a,b,d,e},{a,b,c}
-s[0]~s[n] > 각 길이 계산 후 짧은 것부터 나열 > 다음 길이 집합에서 앞에원소 제거 후 추가
-
 def solution(s):
-    s_lst = list(s[].split(','))
+    answer = []
+    s = list(s[2:-2].split('},{'))  # len(s)-2는 -2만 적어도 된다.
+    s = sorted(s, key=lambda x: len(x))  # ['3', '2,3', '4,2,3', '2,3,4,1']
+
+    s_lst = []
+    for n in s:
+        s_lst.append(list(map(int, n.split(','))))  # [[3], [2, 3], [4, 2, 3], [2, 3, 4, 1]]
+
     for i in range(len(s_lst)):
-        s_lst[i] = s_lst.strip('{','}')
-    i=0
-    while i < len(s_lst)-1:
-        if len(s_lst[i]) > len(s_lst[i+1]):
-            temp = s_lst[i]
-            s_lst[i] = s_lst[i+1]
-            s_lst[i+1] = temp
-            if i >= 2:
-                i -= 2
-    # 길이별 정렬된 리스트
-    각 원소가 튜플
+        for j in range(len(s_lst[i])):
+            if s_lst[i][j] not in answer:
+                answer.append(s_lst[i][j])
+                break
+
+    return answer
